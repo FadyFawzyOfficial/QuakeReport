967c8a1f58f46211fd4d4f47d87809ee02a955cc
Fady Fawzi <Engineer.FadyFawzi@gmail.com>
2020-02-17 21:25:27 +0200

    1.6 Add magnitude circle

--- app/src/main/java/com/engineerfadyfawzi/quakereport/EarthquakeAdapter.java
+++ app/src/main/java/com/engineerfadyfawzi/quakereport/EarthquakeAdapter.java
@@ -1,6 +1,7 @@
 package com.engineerfadyfawzi.quakereport;
 
 import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +12,8 @@ import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
+
+import androidx.core.content.ContextCompat;
 
 /**
  * An {@link EarthquakeAdapter} knows how to create a list item layout for each earthquake
@@ -59,12 +62,18 @@ public class EarthquakeAdapter extends ArrayAdapter< Earthquake >
         
         // Find the TextView with view ID magnitude
         TextView magnitudeView = listItemView.findViewById( R.id.magnitude );
-        
         // Format the magnitude to show 1 decimal place
         String formattedMagnitude = formatMagnitude( currentEarthquake.getMagnitude() );
-        
         // Display the magnitude of the current earthquake in that TextView
         magnitudeView.setText( formattedMagnitude );
+        
+        // Set the proper background color on the magnitude circle.
+        // Fetch the background from the TextView, which is a GradientDrawable.
+        GradientDrawable magnitudeCircle = ( GradientDrawable ) magnitudeView.getBackground();
+        // Get the appropriate background color based on the current earthquake magnitude
+        int magnitudeColor = getMagnitudeColor( currentEarthquake.getMagnitude() );
+        // Set the color on the magnitude circle
+        magnitudeCircle.setColor( magnitudeColor );
         
         // Get the original location string from the current Earthquake object,
         // which can be in the format of "5km N of Cairo, Egypt" or "Pacific-Antarctic Ridge".
@@ -137,6 +146,53 @@ public class EarthquakeAdapter extends ArrayAdapter< Earthquake >
     {
         DecimalFormat magnitudeFormat = new DecimalFormat( "0.0" );
         return magnitudeFormat.format( magnitude );
+    }
+    
+    /**
+     * Return the color for the magnitude circle based on the intensity of the earthquake.
+     *
+     * @param magnitude of the earthquake
+     */
+    private int getMagnitudeColor( double magnitude )
+    {
+        int magnitudeColorResourceId;
+        int intMagnitude = ( int ) magnitude;
+        
+        switch ( intMagnitude )
+        {
+            case 0:
+            case 1:
+                magnitudeColorResourceId = R.color.magnitude1;
+                break;
+            case 2:
+                magnitudeColorResourceId = R.color.magnitude2;
+                break;
+            case 3:
+                magnitudeColorResourceId = R.color.magnitude3;
+                break;
+            case 4:
+                magnitudeColorResourceId = R.color.magnitude4;
+                break;
+            case 5:
+                magnitudeColorResourceId = R.color.magnitude5;
+                break;
+            case 6:
+                magnitudeColorResourceId = R.color.magnitude6;
+                break;
+            case 7:
+                magnitudeColorResourceId = R.color.magnitude7;
+                break;
+            case 8:
+                magnitudeColorResourceId = R.color.magnitude8;
+                break;
+            case 9:
+                magnitudeColorResourceId = R.color.magnitude9;
+                break;
+            default:
+                magnitudeColorResourceId = R.color.magnitude10plus;
+        }
+        
+        return ContextCompat.getColor( getContext(), magnitudeColorResourceId );
     }
     
     /**
