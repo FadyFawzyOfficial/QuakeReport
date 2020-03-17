3206064f699f9b76ba3d1e672ed8fcca78a75f43
Fady Fawzi <Engineer.FadyFawzi@gmail.com>
2020-03-17 13:49:57 +0200

    1.14.2 Create the SettingsActivity class (SettingsActivity.java) and Define a layout for it (the SettingsActivity) at settings_activity.xml

---
+++ app/src/main/java/com/engineerfadyfawzi/quakereport/SettingsActivity.java
@@ -0,0 +1,21 @@
+package com.engineerfadyfawzi.quakereport;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SettingsActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate( Bundle savedInstanceState )
+    {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.settings_activity );
+    }
+    
+    public static class EarthquakePreferenceFragment extends PreferenceFragment
+    {
+    
+    }
+}
