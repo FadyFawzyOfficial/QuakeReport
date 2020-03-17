dd28dd7dfd9c1da3a9b615559bf9c300b47f3e5a
Fady Fawzi <Engineer.FadyFawzi@gmail.com>
2020-03-17 13:08:35 +0200

    1.14 Add settings activity

--- app/src/main/java/com/engineerfadyfawzi/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/engineerfadyfawzi/quakereport/EarthquakeActivity.java
@@ -12,6 +12,8 @@ import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -194,5 +196,27 @@ public class EarthquakeActivity extends AppCompatActivity implements LoaderCallb
         // COMPLETED: Loader reset, so we can clear out our existing data.
         // Clear the adapter of previous earthquake data
         mAdapter.clear();
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu( Menu menu )
+    {
+        getMenuInflater().inflate( R.menu.main, menu );
+        return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected( MenuItem item )
+    {
+        int id = item.getItemId();
+        
+        if ( id == R.id.action_settings )
+        {
+            Intent settingsIntent = new Intent( this, SettingsActivity.class );
+            startActivity( settingsIntent );
+            return true;
+        }
+        
+        return super.onOptionsItemSelected( item );
     }
 }
