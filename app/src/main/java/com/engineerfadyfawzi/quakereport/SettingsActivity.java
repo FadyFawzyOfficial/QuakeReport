a7d61d39ccd856c8712e9efd2457dac5cf3d3f96
Fady Fawzi <Engineer.FadyFawzi@gmail.com>
2020-03-17 17:11:40 +0200

    1.15.3 in the SettingsActivity, within the EarthquakePreferenceFragment inner class, Override the onCreate() method to use the settings_main XML resource that defined earlier.

--- app/src/main/java/com/engineerfadyfawzi/quakereport/SettingsActivity.java
+++ app/src/main/java/com/engineerfadyfawzi/quakereport/SettingsActivity.java
@@ -16,6 +16,11 @@ public class SettingsActivity extends AppCompatActivity
     
     public static class EarthquakePreferenceFragment extends PreferenceFragment
     {
-    
+        @Override
+        public void onCreate( Bundle savedInstanceState )
+        {
+            super.onCreate( savedInstanceState );
+            addPreferencesFromResource( R.xml.settings_main );
+        }
     }
 }
